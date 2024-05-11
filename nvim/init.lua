34d2083ee7695a4115b5287aa54dc25ad77cbfff
Galacsh <galacsh.dev@gmail.com>
2024-05-11 15:50:52 +0900

    fix: disable unused providers

--- nvim/init.lua
+++ nvim/init.lua
@@ -5,6 +5,11 @@ require("config.vim")
 require("config.autocmd")
 require("config.lazy")
 
+vim.g.loaded_node_provider = 0
+vim.g.loaded_perl_provider = 0
+vim.g.loaded_python3_provider = 0
+vim.g.loaded_ruby_provider = 0
+
 --[[
   Useful help docs:
   - :help vim.opt
