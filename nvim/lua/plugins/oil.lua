db45a9b38c9883fd7b3094c9b42535b11a245ea5
Galacsh <galacsh.dev@gmail.com>
2024-05-11 14:24:30 +0900

    feat: prevent closing when there is no buffer to go back

--- nvim/lua/plugins/oil.lua
+++ nvim/lua/plugins/oil.lua
@@ -35,5 +35,47 @@ return {
         },
       }
     end,
+    config = function(_, opts)
+      local oil = require("oil")
+
+      --[[
+        Overrides oil.nvim's close method.
+        Prevents closing it `bprev` fails.
+
+        See: https://github.com/stevearc/oil.nvim/blob/master/lua/oil/init.lua#L411
+      --]]
+      oil.close = function()
+        if vim.w.is_oil_win then
+          local original_winid = vim.w.oil_original_win
+          vim.api.nvim_win_close(0, true)
+          if original_winid and vim.api.nvim_win_is_valid(original_winid) then
+            vim.api.nvim_set_current_win(original_winid)
+          end
+          return
+        end
+        local ok, bufnr = pcall(vim.api.nvim_win_get_var, 0, "oil_original_buffer")
+        if ok and vim.api.nvim_buf_is_valid(bufnr) then
+          vim.api.nvim_win_set_buf(0, bufnr)
+          if vim.w.oil_original_view then
+            vim.fn.winrestview(vim.w.oil_original_view)
+          end
+          return
+        end
+
+        local oilbuf = vim.api.nvim_get_current_buf()
+        ok = pcall(vim.cmd.bprev)
+        if not ok then
+          -- ===============================================
+          -- == Send notification and return immediately. ==
+          -- ===============================================
+          vim.notify("No more buffers to switch to.", vim.log.levels.INFO)
+          return
+        end
+        vim.api.nvim_buf_delete(oilbuf, { force = true })
+      end
+
+      oil.setup(opts)
+    end
   }
 }
+
