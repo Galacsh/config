8dbae10a6bbdb34ee55a85fde8fd036692f1a1f8
Galacsh <galacsh.dev@gmail.com>
2024-05-11 20:57:34 +0900

    chore: specify file extension

--- nvim/lua/config/vim.lua
+++ nvim/lua/config/vim.lua
@@ -1,7 +1,7 @@
 -- [[ Source Vim configuration files ]] --
 
-vim.cmd([[source ~/.vim/common_options]])
-vim.cmd([[source ~/.vim/common_keymap]])
-vim.cmd([[source ~/.vim/vim_series_options]])
-vim.cmd([[source ~/.vim/vim_series_keymap]])
+vim.cmd([[source ~/.vim/common_options.vimrc]])
+vim.cmd([[source ~/.vim/common_keymap.vimrc]])
+vim.cmd([[source ~/.vim/vim_series_options.vimrc]])
+vim.cmd([[source ~/.vim/vim_series_keymap.vimrc]])
 
