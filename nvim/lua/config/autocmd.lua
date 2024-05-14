8130942fe92bb4973f2e140836cd454965fed1c2
Galacsh <galacsh.dev@gmail.com>
2024-05-14 23:39:43 +0900

    fix: conceal level of json files

--- nvim/lua/config/autocmd.lua
+++ nvim/lua/config/autocmd.lua
@@ -3,12 +3,20 @@
 local augroup = vim.api.nvim_create_augroup
 local autocmd = vim.api.nvim_create_autocmd
 
--- highlight on yank
 autocmd("TextYankPost", {
 	desc = "Highlight when yanking (copying) text",
 	group = augroup("highlight-yank", { clear = true }),
 	callback = function()
 		vim.highlight.on_yank()
 	end,
+})
+
+autocmd("FileType", {
+	desc = "Fix conceallevel for json files",
+  group = augroup("json_conceal", { clear = true }),
+  pattern = { "json", "jsonc", "json5" },
+  callback = function()
+    vim.opt_local.conceallevel = 0
+  end,
 })
 
